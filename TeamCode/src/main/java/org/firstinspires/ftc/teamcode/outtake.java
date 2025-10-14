43ca757b14843ec68e3afc7faf1abc9311340507
L-Logix <112667606+L-Logix@users.noreply.github.com>
2025-10-14 16:19:18 -0400

    Update outtake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outtake.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
+
 @TeleOp(name = "Shooter Test", group = "Practice")
 public class outtake extends OpMode {
 
