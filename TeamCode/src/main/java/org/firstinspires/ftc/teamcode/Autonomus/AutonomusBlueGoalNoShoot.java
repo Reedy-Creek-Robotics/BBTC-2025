f01b133a2284f8c4d09012cc86ec4f72cb86499f
ojasgodbole <godboleojas@gmail.com>
2026-01-16 00:48:41 -0500

    commit_1 1_16_26

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomus/AutonomusBlueGoalNoShoot.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Autonomus;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auto : Blue Goal NO-shoot")
+public class AutonomusBlueGoalNoShoot extends BaseAutonomus {
+
+
+    @Override
+    public void runOpMode() {
+
+        initializeHardware();
+
+        telemetry.addLine("Ready!");
+        telemetry.update();
+
+        waitForStart();
+
+        if (!opModeIsActive()) return;
+
+        // -------------------------------
+        // AUTONOMOUS STEPS
+        // -------------------------------
+
+        sleep(22);
+        moveForward(3, 0.6);
+        rotate(55, TURN_SPEED);
+        moveForward(8, DRIVE_SPEED);
+
+        // End
+        stopAll();
+    }
+}
